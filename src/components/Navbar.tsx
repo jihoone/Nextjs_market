35ccca0734ec252dce03370b41e72dc718b7fb44
정지훈 <jungjihoons@naver.com>
2023-08-02 22:16:17 +0900

    #5 - getServerSession
    이전 컴포넌트에서 유저 세션데이터를 사용하기 위해 useSession을 이용해서 사용중
    -> useSession은 클라이언트 컴포넌트에서 사용 가능
    -> getServerSession 함수를 이용하면 서버 컴포넌트에서 세션 데이터를 가져오는 것 가능
    
    => next버전 13.4 이전에는 서버 컴포넌트에서 클라이언트 컴포넌트로 props를 전달해줄 때 `createdAt`, `updatedAt` 타입이 String이 아니라 date object 타입이라 전달 할 수 없다는 오류 (not supported)
    -> `createdAt: currentUser.createdAt.toIString()` return 값을 String으로 변경해서 사용
    => but next 13.4 이후 버전은 가능

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,15 @@
 import Link from 'next/link'
 import React, { useState } from 'react'
 import NavItem from './NavItem';
+import { User } from '@prisma/client';
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
   const [menu, setMenu] = useState(false);
+  console.log('currentUser', currentUser);
 
   const handleMenu =() => {
     setMenu(!menu);
@@ -23,12 +29,12 @@ const Navbar = () => {
         </div>
 
         <div className='hidden sm:block'>
-          <NavItem />
+          <NavItem currentUser={currentUser} />
         </div>
       </div>
 
       <div className='block sm:hidden'>
-        {(menu === false) ? null : <NavItem mobile />}
+        {(menu === false) ? null : <NavItem mobile currentUser={currentUser} />}
       </div>
     </nav>
   )
